c11fb0d70b71dd765295f4b0f538872935779263
podslushanonl <podslushano.nl@gmail.com>
2026-08-25 18:49:58 +0200

    Install action-post content layer

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,10 @@
 # Подключаем патч месячной афиши при загрузке пакета handlers.
 # Модуль не вызывает AI: он только находит официальный сайт/фото HTTP-запросами.
 from handlers import photo_refresh as _photo_refresh  # noqa: F401,E402
+
+# Контент-центр: два action-поста в неделю вместо старых коротких напоминаний.
+# Импорт content здесь намеренный: бот затем получает уже настроенный модуль.
+from handlers import content as _content  # noqa: F401,E402
+from utils.action_content import install_action_templates as _install_action_templates  # noqa: E402
+
+_install_action_templates()
