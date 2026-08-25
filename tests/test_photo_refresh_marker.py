ef53f0a26c48b5c71e618843f55c59dbadeeb631
podslushanonl <podslushano.nl@gmail.com>
2026-08-25 15:39:02 +0200

    Обновить marker тест для photo v3

--- tests/test_photo_refresh_marker.py
+++ tests/test_photo_refresh_marker.py
@@ -1,5 +1,5 @@
 from handlers.photo_refresh import PHOTO_VERSION
 
 
-def test_photo_version_is_v2():
-    assert PHOTO_VERSION == "official-photo-v2"
+def test_photo_version_is_v3():
+    assert PHOTO_VERSION == "official-photo-v3"
